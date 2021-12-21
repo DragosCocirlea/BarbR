5e90a2119c5e102bbf45027db09fa2bb4ab604da
DragosCocirlea <dragos.cocirlea@gmail.com>
2021-12-21 22:48:15 +0200

    Added authentication and networkUtils

---
+++ app/src/main/java/ro/akaaka/barbr/activities/Main/MainActivity.kt
@@ -0,0 +1,13 @@
+package ro.akaaka.barbr.activities.Main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ro.akaaka.barbr.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
